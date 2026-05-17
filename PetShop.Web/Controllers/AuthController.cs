332878827428f931ca0c83221465f015a57fa2cf
Adriana <adriana.otte@outlook.com>
2026-05-17 02:11:17 -0300

    feat: adiciona tela de login e ajustes navbar

--- PetShop.Web/Controllers/AuthController.cs
+++ PetShop.Web/Controllers/AuthController.cs
@@ -2,16 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace PetShop.Web.Controllers
 {
-	public class AuthController : Controller
-	{
-		public IActionResult Login()
-		{
-			return View();
-		}
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-		public IActionResult Register()
-		{
-			return View();
-		}
-	}
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            return RedirectToAction("Dashboard", "Admin");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
 }
